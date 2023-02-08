d9e150c49ad6693d8227a9ca8cacca44ff58e4a1
Marco Poggi <marcopoggi2017@gmail.com>
2023-02-08 16:09:45 +0000

    feat - add realtime actions

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,41 +1,52 @@
 class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
+  # GET /quotes
   def index
-    @quotes = Quote.all
+    @quotes = Quote.ordered_newest
   end
 
-  def show
-  end
+  # GET /quotes/:id
+  def show; end
 
+  # GET /quotes/new
   def new
     @quote = Quote.new
   end
-
+  
+  # POST /quotes/
   def create
     @quote = Quote.new(quote_params)
 
     if @quote.save
-      redirect_to quotes_path, notice: "Quote was successfully created."
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote successfully created." }
+        format.turbo_stream
+      end
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, notice: "Quote was successfully updated."
+      redirect_to quotes_path, notice: "Quote successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @quote.destroy
-    redirect_to quotes_path, notice: "Quote was successfully destroyed."
+    if @quote.destroy
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote successfully deleted." }
+        format.turbo_stream
+      end
+    else
+      render quotes_path, status: :unprocessable_entity
+    end
   end
 
   private
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,29 @@
 class Quote < ApplicationRecord
-		validates :name, presence: true
+  validates :name, presence: true
+
+  # --- scopes ---
+
+  scope :ordered_newest, -> { order(id: :desc) }
+
+  # --- callbacks ---
+
+  # OPTION 1
+
+  # after_create_commit -> {
+  #   broadcast_prepend_to(
+  #     "quotes", #stream channel
+  #     partial: "quotes/quote", locals: { quote: self } # what will be inserted?
+  #     target: "quotes" # which turbo_trame?
+  #   )
+  # }
+
+  # OPTION 2
+
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_update_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  #OPTION 3
+
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
