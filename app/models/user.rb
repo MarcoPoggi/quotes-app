0ecba8927830472b00903e8212bdcaafcf28a770
Marco Poggi <marcopoggi2017@gmail.com>
2023-02-28 18:37:42 -0300

    wip - devise / flash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable
+  VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_PASSWORD_REGEXP = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}\z/
 
+  # --- RELATIONSHIP
+  devise :database_authenticatable, :registerable, :recoverable
   belongs_to :company
 
-  validates :email, presence: true
+  # --- VALIDATIONS
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 8 },
+                       format: { with: VALID_PASSWORD_REGEXP, message: "must contains least one(number, lower case, capital letter)" },
+                       confirmation: true
 
+  # --- METHODS
   def name #decorator?
     "@#{email.split("@").first.split("+").first.downcase}"
   end
